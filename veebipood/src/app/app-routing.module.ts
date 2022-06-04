cc002dbad6505b7ffc515fcac6d32d64bc8614b6
mihkelvaher <mihkel.vaher@industry62.com>
2022-06-04 14:32:35 +0300

    ng generate component, routerLink, path+component

--- veebipood/src/app/app-routing.module.ts
+++ veebipood/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvalehtComponent } from './avaleht/avaleht.component';
+import { OstukorvComponent } from './ostukorv/ostukorv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // localhost:4200/  ---> avaleht.component.html sisu 
+  //            ja saab kaasa .ts dünaamilisuse jaoks ja .css kujunduse jaoks
+  { path: "", component: AvalehtComponent },
+  { path: "ostukorv", component: OstukorvComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- veebipood/src/app/app.module.ts
+++ veebipood/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AvalehtComponent } from './avaleht/avaleht.component';
+import { OstukorvComponent } from './ostukorv/ostukorv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvalehtComponent,
+    OstukorvComponent
   ],
   imports: [
     BrowserModule,
