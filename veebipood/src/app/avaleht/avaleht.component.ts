1b8df53acc28b3641ed4f30818758a91797a2bbb
mihkelvaher <mihkel.vaher@industry62.com>
2022-06-06 15:59:42 +0300

    ts ja html vaheline suhtlus

--- veebipood/src/app/avaleht/avaleht.component.ts
+++ veebipood/src/app/avaleht/avaleht.component.ts
@@ -5,11 +5,71 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './avaleht.component.html',
   styleUrls: ['./avaleht.component.css']
 })
-export class AvalehtComponent implements OnInit {
+export class AvalehtComponent implements OnInit { // klassi lahti mineku loogeline sulg
+  s6naline = "Nimi"; // toodete nimi, kategooria, pilt, kirjeldus    isikutel nimi, postiindeks, isikukood, tel nr 
+  numbriline = 1312312; // toote hinnad, kogused      inimestel lapse arv
+  kahendV22rtus = true; // tõde või väär ja kolmandat varianti pole.   toode on aktiivne, toode on laos, isik täisealine, sisselogitud
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Läksin avalehe lehele");
   }
 
-}
+  muudaS6nalineV22rtus() {
+    this.s6naline = "Uus nimi";
+  }
+
+  muudaS6nalineV22rtusOriginaali() {
+    this.s6naline = "Tagasi nimi";
+  }
+
+  muudaNumbrilineV22rtus() {
+    this.numbriline = 3123;
+  }
+
+  muudaNumbrilineV22rtusOriginaali() {
+    this.numbriline = 1312312;
+  }
+
+  muudaKahendV22rtus() {
+    this.kahendV22rtus = false;
+  }
+
+  muudaKahendV22rtusOriginaali() {
+    this.kahendV22rtus = true;
+  }
+
+  aktiivsuseMuutus() { // funktsiooni lahti mineku loogeline
+    this.kahendV22rtus = !this.kahendV22rtus;
+  } // funktsiooni kinni mineku loogeline sulg
+
+  korrutaNumberKahega() {
+    console.log(this.numbriline);
+    this.numbriline = this.numbriline * 2;
+    console.log(this.numbriline);
+  }
+
+  muudaK6ik() {
+    this.kahendV22rtus = true;
+    this.numbriline = this.numbriline + 3112312;
+    this.s6naline = "Jutumärkide sees oleva uue väärtuse";
+  }
+  
+} // klassi kinni mineku loogeline sulg
+
+// salvestus
+// 1. andmebaas - pilveteenusesse
+// 2. brauseri vahemälu - chrome/firefox/edge
+// 3. fail - logid
+
+
+// tumesinine - selle klassiga seotud asjad
+// helesinine - muutujad
+//  kollane - funktsioon
+
+
+// muutujate väärtused:
+// oranž - sõnaline
+// roheline - numbriline
+// tumesinine - kahendväärtus
--- veebipood/src/app/ostukorv/ostukorv.component.ts
+++ veebipood/src/app/ostukorv/ostukorv.component.ts
@@ -10,6 +10,7 @@ export class OstukorvComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Läksin ostukorvi lehele");
   }
 
 }
